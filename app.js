1c7f4219b0824f6953400106c3ce4ad034b75179
Flavio Visnardi Filho <flaviovisnardifilho@gmail.com>
2023-04-05 14:31:23 -0300

    add example of mongoose connection on app

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ const app = express();
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-// const { mainModule } = require("process");
 
+// Set up mongoose connection
+const dev_db_url =
+  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 
 main().catch((err) => console.log(err));
 async function main() {
